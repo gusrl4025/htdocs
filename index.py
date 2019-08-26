0b99af7a7f1d62dab5efff8fab7a5a905838dfca
gusrl4025 <gusrl4025@naver.com>
2019-08-26 21:18:43 +0900

    목차 자동생성

--- index.py
+++ index.py
@@ -1,15 +1,20 @@
 #!C:\Users\gusrl\AppData\Local\Programs\Python\Python37-32\python.exe
 print("Content-Type: text/html")
 print()
-import cgi
+import cgi, os
+
+files = os.listdir('data')
+print(files)
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
 form = cgi.FieldStorage()
 if "id" in form:
     pageId = form["id"].value
-    description = open('data/'+pageId, 'r',encoding='utf8').read()
+    description = open('data/'+pageId, 'r',encoding='UTF-8').read()
 else:
     pageId = 'Welcome'
-    description = open('data/GameEngine','r',encoding='utf8').read()
-print(pageId)
+    description = open('main_data/GameEngine','r',encoding='UTF-8').read()
 print('''<!DOCTYPE html>
 <html>
 <head>
@@ -19,12 +24,10 @@ print('''<!DOCTYPE html>
 <body>
     <h1><a href = "index.py">GameEngine</a></h1>
     <ol>
-        <li><a href = "index.py?id=Unity">Unity</a></li>
-        <li><a href = "index.py?id=Unreal">Unreal</a></li>
-        <li><a href = "index.py?id=Cocos2d">Cocos2d</a></li>
+        {listStr}
     </ol>
     <h2>{title}</h2>
     {desc}
 </body>
 </html>
-'''.format(title=pageId,desc=description))
+'''.format(title=pageId,desc=description,listStr=listStr))
