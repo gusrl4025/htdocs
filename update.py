9ee9509bc0e6f70bca0f471c4fc5a1714bf8bab1
gusrl4025 <gusrl4025@naver.com>
2019-09-06 12:01:15 +0900

    2019-09-06

--- update.py
+++ update.py
@@ -2,20 +2,18 @@
 print("Content-Type: text/html")
 print()
 import cgi, os
-
 files = os.listdir('data')
 listStr = ''
 for item in files:
     listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+
 form = cgi.FieldStorage()
 if "id" in form:
     pageId = form["id"].value
     description = open('data/'+pageId, 'r',encoding='UTF-8').read()
-    update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
 else:
     pageId = 'Welcome'
     description = open('main_data/GameEngine','r',encoding='UTF-8').read()
-    update_link = ''
 print('''<!DOCTYPE html>
 <html>
 <head>
@@ -28,9 +26,12 @@ print('''<!DOCTYPE html>
         {listStr}
     </ol>
     <a href="create.py">create</a>
-    {update_link}  
-    <h2>{title}</h2>
-    {desc}
+    <form action="process_create.py" method="post">
+        <input type="hidden" name="pageId" value="{form_default_title}">
+        <p><input type="text" name="title" placeholder="title" value="{form_default_title}"></p>
+        <p><textarea rows="4" name="description" placeholder="description">{form_default_description}</textarea></p>
+        <p><input type="submit"></p>
+    </form>
 </body>
 </html>
-'''.format(title=pageId,desc=description,listStr=listStr,update_link=update_link))
+'''.format(title=pageId,desc=description,listStr=listStr,form_default_title=pageId,form_default_description=description))
